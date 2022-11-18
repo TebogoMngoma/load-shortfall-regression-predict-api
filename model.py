5f26510329f96fa66121379fda66826b789ad69b
TebogoMngoma <tebogo.mngoma@live.com>
2022-11-18 17:09:56 +0200

    ss

--- model.py
+++ model.py
@@ -60,14 +60,18 @@ def _preprocess_data(data):
 
 
     # ----------- Replace this code with your own preprocessing steps --------
-    for column in feature_vector_df:
-        with_nulls = feature_vector_df[column].isna().sum()
-        if with_nulls > 0 :
-            feature_vector_df = feature_vector_df.drop(column,axis=1)
-        else:
-            if column not in feature_vector_df.select_dtypes(include='number').columns:
-                feature_vector_df = feature_vector_df.drop(column,axis=1)
-    predict_vector = feature_vector_df
+    
+    predict_vector = feature_vector_df[[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
+       'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
+       'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
+       'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
+       'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h',
+       'Seville_pressure', 'Seville_rain_1h', 'Bilbao_snow_3h',
+       'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h',
+       'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
+       'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id',
+       'Valencia_pressure', 'Seville_temp_max', 'Bilbao_weather_id', 
+        'Valencia_humidity', 'Year', 'Month', 'Day','Hour']]]
     # ------------------------------------------------------------------------
 
     return predict_vector
